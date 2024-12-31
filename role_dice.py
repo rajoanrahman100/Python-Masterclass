90b303bc0d66640f0dc09c355aa242006305dc1a
rajoanrahman100 <rajoantikweb@gmail.com>
2024-12-31 10:10:54 +0600

    role the dice example

---
+++ role_dice.py
@@ -0,0 +1,18 @@
+import random
+
+
+class Dice:
+    def rollDice(self):
+    
+        first = random.randint(1, 6)
+        second=random.randint(1,6)
+        
+        return first,second
+    
+    
+dice=Dice()
+
+print(dice.rollDice())    
+
+
+
