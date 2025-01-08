0100eff59a96f9b7065686a579c75cdb764929d2
rajoanrahman100 <rajoantikweb@gmail.com>
2025-01-08 22:52:13 +0600

    file changes

--- PythonWebServer/app.py
+++ PythonWebServer/app.py
@@ -6,10 +6,15 @@ async def app(scope, receive, send):
         'type': 'http.response.start', # Indicate that the response headers are being sent
         'status': 200, # HTTP status code
         'headers': [ # Response headers
-            [b'content-type', b'text/plain'], # Content-Type: text/plain
+            [b'content-type', b'text-plain'], # Content-Type: text/plain
         ],
     })
     await send({ # Send the response body
         'type': 'http.response.body', # Indicate that the response body is being sent
         'body': b'Hello, world! Ihan', # Response body
     })
+    
+def userName(firstname:str, lastname:str):
+    return firstname
+
+print(userName("matti", "meikäläinen"))   
